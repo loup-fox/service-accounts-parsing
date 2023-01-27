320bfed869258be9dc392126c1d3c6046e741d19
loup-fox <loup@foxintelligence.fr>
2023-01-27 23:34:02 +0100

    wip

--- src/process/ProcessAccount.ts
+++ src/process/ProcessAccount.ts
@@ -29,34 +29,26 @@ export const ProcessAccount =
       Logger.error("Error finding account", account.error);
       return;
     }
-
-    const mails = await getNewMails(accountId);
-    if (mails.length === 0) {
-      Logger.info(`No new mails for account ${accountId}.`);
-      return;
-    }
-
-    Logger.info(`Processing ${mails.length} mails for account ${accountId}...`);
-
     const credentials = await decryptPayload(account.value.payload);
     if (!credentials.success) {
       Logger.error("Error decrypting credentials", credentials.error);
       return;
     }
 
-    const $process = from(fetchMails(credentials.value, mails)).pipe(
+    const $process = from(getNewMails(accountId)).pipe(
+      mergeMap((mails) => fetchMails(credentials.value, mails)),
       bufferCount(50),
       map((mails) => mails.filter(isSuccess).map((x) => x.value)),
       mergeMap((mails) =>
         from(mails).pipe(
           mergeMap((mail) => parseMail(account.value, mail)),
           filter(isSuccess),
-          map((x) => x.value)
+          map(({ value }) => value)
         )
       ),
       bufferCount(50),
       mergeMap((documents) => {
-        Logger.info(`Writing ${mails.length} documents to BigQuery...`);
+        Logger.info(`Writing ${documents.length} documents to BigQuery...`);
         return writeToBq(documents);
       }),
       map((result) => {
